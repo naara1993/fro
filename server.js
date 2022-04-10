abe84b04ce1f12b8e3f2c247fa7e828470727673
naara cataldo <naarabelencataldo@outlook.com>
2022-04-10 18:16:28 -0300

    =sdsadsad

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/portafoliofinal-main'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/portafoliofinal-main/index.html'));
+});
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
---
+++ src/app/seccion-contenedor/seccion-contenedor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion-contenedor',
+  templateUrl: './seccion-contenedor.component.html',
+  styleUrls: ['./seccion-contenedor.component.css']
+})
+export class SeccionContenedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
